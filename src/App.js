194c7502974651729de4413ef3e672495689eaa4
Nadya Kandakova <nadyawinters@gmail.com>
2020-03-03 15:47:36 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
+//import Search from "./Search";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="recipes">
+        {recipes.map((recipe, index) => (
+          <Recipe
+            key={index}
+            title={recipe.recipe.label}
+            dietLabels={recipe.recipe.dietLabels}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./recipe.module.css";
+
+const Recipe = ({ title, dietLabels, image, ingredients }) => {
+  return (
+    <div className={style.recipe}>
+      <h1>{title}</h1>
+      <img src={image} alt="" />
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient.text}</li>
+        ))}
+      </ul>
+
+      {dietLabels.map((dietLabel, index) => (
+        <p key={index}>*{dietLabel}</p>
+      ))}
+    </div>
+  );
+};
+export default Recipe;
