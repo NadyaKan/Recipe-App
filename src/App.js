4d4eaf6b13156f941902c4713f945655350acd8f
Nadya Kandakova <nadyawinters@gmail.com>
2020-03-03 17:09:10 +0100

    redux practice

--- src/App.js
+++ src/App.js
@@ -4,8 +4,51 @@ import Recipe from "./Recipe";
 //import Search from "./Search";
 
 const App = () => {
+  //////
+  const APP_ID = "cc720f00";
+  const APP_KEY = "e2fc9c5cb39d3ee9c962257da1ac8be4";
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    // const response = await fetch(
+    //   `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    // );
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+  ///////////
+
   return (
     <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+
       <div className="recipes">
         {recipes.map((recipe, index) => (
           <Recipe
