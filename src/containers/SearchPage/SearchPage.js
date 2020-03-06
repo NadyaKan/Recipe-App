63251661c01a519f845421ec3026bf861f0f6cbd
Nadin <nadyawinters@gmail.com>
2020-03-06 21:36:39 +0100

    added a link to bookmarks on the searchPage

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -1,10 +1,18 @@
 import { useSelector } from "react-redux";
 import React from "react";
+import { Link } from "react-router-dom";
 import Search from "../../components/Search/Search";
 
 const SearchPage = () => {
   const isSearching = useSelector(state => state.recipes.gettingRecipes);
-  return isSearching ? <h4>Loading...</h4> : <Search />;
+  return (
+    <div>
+      <div>
+        <Link to="/bookmarks">My Bookmarks</Link>
+      </div>
+      {isSearching ? <h4>Loading...</h4> : <Search />}
+    </div>
+  );
 };
 
 export default SearchPage;
